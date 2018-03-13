f41a9581ec92f1d98e5e991c00f809cdda1145b0
Greg <greg@Gregs-MacBook-Pro.local>
2018-03-13 07:50:06 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Going to use this repo to test weird React.Child issues that I'm having</h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
