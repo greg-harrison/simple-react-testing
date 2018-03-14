477db41501df53a6624cb07fae3b33671094a75d
Greg <greg@Gregs-MacBook-Pro.local>
2018-03-14 07:21:59 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardSection } from './components/cardSection/CardSection'
+import { CardItem } from './components/cardItem/CardItem'
+import map from 'lodash/map'
+import { cardData } from './assets/data'
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      cardData: cardData
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,6 +22,16 @@ class App extends Component {
           <h2>Going to use this repo to test weird React.Child issues that I'm having</h2>
           <div>Had to get the dev branch set up</div>
         </header>
+
+        <CardSection>
+          {
+            map(this.state.cardData, (card) => {
+              return (
+                <CardItem id={card.id} title={card.title} />
+              )
+            })
+          }
+        </CardSection>
       </div>
     );
   }
