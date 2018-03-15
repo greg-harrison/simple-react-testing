30c3f9f914074af2669471009b626aa7922d44a6
Greg <greg@Gregs-MacBook-Pro.local>
2018-03-15 07:20:18 -0400

    Removed Nested Map experiment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { CardSection } from './components/cardSection/CardSection'
-import { CardItem } from './components/cardItem/CardItem'
 import map from 'lodash/map'
 import { cardData } from './assets/data'
 
@@ -18,20 +16,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h2>Going to use this repo to test weird React.Child issues that I'm having</h2>
-          <div>Had to get the dev branch set up</div>
-        </header>
-
-        <CardSection>
-          {
-            map(this.state.cardData, (card) => {
-              return (
-                <CardItem id={card.id} title={card.title} />
-              )
-            })
-          }
-        </CardSection>
       </div>
     );
   }
--- src/components/cardSection/CardSection.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const CardSection = ({ children }) => {
-    return (
-        <div className="cardSection">
-            {children}
-        </div>
-    )
-}
