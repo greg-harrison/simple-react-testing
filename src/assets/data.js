281b36e3dd60ce0e60bd9ba7fbdb8c3de017ac3b
Greg <greg@Gregs-MacBook-Pro.local>
2018-03-22 07:32:08 -0400

    Using Javascript's built-in map, instead of pulling in Lodash

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,18 +1,22 @@
 export const cardData = [
-    {
-        id: "230203203",
-        title: 'Hello'
-    },
-    {
-        id: "23020320393",
-        title: 'World'
-    },
-    {
-        id: "230203203932329",
-        title: 'Thing'
-    },
-    {
-        id: "230203203932330",
-        title: 'Thing2'
-    }
+  {
+    id: "230203203",
+    title: 'Hello',
+    description: 'Testing'
+  },
+  {
+    id: "23020320393",
+    title: 'World',
+    description: 'Here we go'
+  },
+  {
+    id: "230203203932329",
+    title: 'Thing',
+    description: 'Nice'
+  },
+  {
+    id: "230203203932330",
+    title: 'Thing2',
+    description: 'Okay'
+  }
 ]
