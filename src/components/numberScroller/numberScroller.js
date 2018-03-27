d1560ada856bce141276dafe3c5a049506b698ca
Greg <greg@Gregs-MacBook-Pro.local>
2018-03-27 07:42:14 -0400

    bit of planning

--- src/components/numberScroller/numberScroller.js
+++ src/components/numberScroller/numberScroller.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 export default class NumberScroller extends React.Component {
+  // Keep rendering and reseting the statevalue until
+  // for loop (x<10)
+  //  update state.num to Math.random values
+  // when x = 10
+  //  set the actual value to the item
   componentDidMount() {
     console.log('mounted');
   }
