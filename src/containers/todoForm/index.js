2052275e780070e3527753410cd55bb2db64624a
Greg <greg@eric-laptop.uq.local>
2018-03-30 09:09:16 -0400

    added in error handling

--- src/containers/todoForm/index.js
+++ src/containers/todoForm/index.js
@@ -5,12 +5,25 @@ import { addTodo } from '../../store/actions'
 const TodoForm = ({ addTodo }) => {
   let titleInput
   let descriptionInput
+  let error = {}
 
   function submitTodo(e) {
     e.preventDefault()
+    error.title = false
+    error.description = false
+
     if (!titleInput.value.trim()) {
       return
     }
+    if (titleInput.value.length > 30) {
+      error.title = true
+      return
+    }
+    if (descriptionInput.value.length > 100) {
+      error.description = true
+      return
+    }
+
     addTodo(titleInput.value, descriptionInput.value)
     titleInput.value = ''
     descriptionInput.value = ''
@@ -20,11 +33,17 @@ const TodoForm = ({ addTodo }) => {
     <div>
       <form className="cardForm" onSubmit={e => submitTodo(e)}>
         <label>
-          <p className="p-0 m-0">Title</p>
+          <p className={"p-0 m-0 " + (!!error.title ? 'error' : '')}>
+            <span>Title</span>
+            {!!error.title && (<span> is too long</span>)}
+          </p>
           <input ref={node => titleInput = node} />
         </label>
         <label>
-          <p className="p-0 m-0">Description</p>
+          <p className={"p-0 m-0 " + !!error.description ? 'error' : ''}>
+            Description
+            {error.description && <span> is too long</span>}
+          </p>
           <input ref={node => descriptionInput = node} />
         </label>
         <button type="submit">
