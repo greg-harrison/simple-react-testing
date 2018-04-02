5a12b5b2957034645f5b64d5fc78711a9f9ea1b8
Greg <greg@Gregs-MacBook-Pro.local>
2018-04-02 07:26:36 -0400

    Removed the components that I was showing just for the sake of seeing them render out

--- src/App.js
+++ src/App.js
@@ -25,25 +25,6 @@ class App extends React.Component {
         <div className="App">
           <div className="container">
             <div className="row">
-              <DisplayCard
-                className="col-6"
-                cardTitle='HorizontalSplit Component'
-              >
-                <HorizontalSplit
-                  className="w-100"
-                  leftSide={(<div> Left Side </div>)}
-                  rightSide={(<div> Right Side </div>)} />
-              </DisplayCard>
-              <DisplayCard
-                className="col-6"
-                cardTitle='Toggle Component'
-              >
-                <Toggle onToggle={
-                  on => console.log('toggle', on)
-                } />
-              </DisplayCard>
-            </div>
-            <div className="row">
               <HorizontalSplit
                 className="w-100"
                 leftSide={(
