c0320bceabcd15b8b7ec9169a0beb7b544a76de0
Greg <greg@Gregs-MacBook-Pro.local>
2018-04-13 08:02:59 -0400

    Added API to redux store

---
+++ src/store/api/index.js
@@ -0,0 +1,36 @@
+import { v4 } from 'node-uuid'
+
+const mockDB = {
+  todos: [
+    {
+      id: v4(),
+      title: 'Test1',
+      description: 'From MockDB',
+      completed: true
+    },
+    {
+      id: v4(),
+      title: 'Test2',
+      description: 'From MockDB',
+      completed: false
+    },
+    {
+      id: v4(),
+      title: 'Test3',
+      description: 'From MockDB',
+      completed: true
+    },
+  ]
+}
+
+const delay = (ms) =>
+  new Promise(resolve => setTimeout(resolve, ms));
+
+export const fetchTodos = () =>
+  delay(500).then(() => {
+    if (Math.random() > 0.5) {
+      throw new Error('Boom!');
+    }
+
+    return mockDB.todos;
+  });
