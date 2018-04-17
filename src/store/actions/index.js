c34111b0470db5521709a942ab80913a74c57a1e
Greg <greg@Gregs-MacBook-Pro.local>
2018-04-17 07:44:52 -0400

    Updates to redux

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,4 +1,31 @@
 import { v4 } from 'node-uuid';
+import * as api from '../api'
+import { getIsFetching } from '../reducers'
+
+export const fetchTodos = () => (dispatch, getState) => {
+  if (getIsFetching(getState())) {
+    return Promise.resolve();
+  }
+
+  dispatch({
+    type: 'FETCH_TODOS_REQUEST',
+  });
+
+  return api.fetchTodos().then(
+    response => {
+      dispatch({
+        type: 'FETCH_TODOS_SUCCESS',
+        response,
+      });
+    },
+    error => {
+      dispatch({
+        type: 'FETCH_TODOS_FAILURE',
+        message: error.message || 'Something went wrong.',
+      });
+    }
+  );
+};
 
 export const addTodo = (title, description) => ({
   type: 'ADD_TODO',
--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -1,7 +1,10 @@
+import { combineReducers } from 'redux'
+import byId, * as fromById from './byId';
+import createList, * as fromList from './createList';
 
 const initialState = []
 
-const todos = (state = initialState, action) => {
+const todoUpdates = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
@@ -24,4 +27,22 @@ const todos = (state = initialState, action) => {
   }
 }
 
+const todos = combineReducers({
+  byId,
+  todoUpdates
+})
+
 export default todos
+
+export const getVisibleTodos = (state) => {
+  const ids = fromList.getIds(createList);
+  return ids.map(id => fromById.getTodo(state.byId, id));
+};
+
+export const getIsFetching = (state) => {
+  fromList.getIsFetching(createList)
+}
+
+export const getErrorMessage = (state) => {
+  fromList.getErrorMessage(createList)
+}
